e4a35fa18f207563cee844fe3c1b5f9f4fb50a9f
Your Name <you@example.com>
2021-06-22 12:05:24 -0500

    music table is complete as well as delete function

--- music_library/src/components/MusicTable/musicTable.jsx
+++ music_library/src/components/MusicTable/musicTable.jsx
@@ -2,43 +2,34 @@ import React from 'react';
 import Delete from '../Delete/delete'
 
 function MusicTable(props){
-    console.log(props);
-    if(props.songs === undefined){
-        return (
-            <div></div>
-        );
-    }
-    else{
-        let musicList = props.songs.map(function(song){
-            return <tr>
-                <td>{song.title}</td>
-                <td>{song.album}</td>
-                <td>{song.artist}</td>
-                <td>{song.genre}</td>
-                <td>{song.release_date}</td>
-                <td>{song.likes}</td>
-                <Delete songid={song.id} deleteSongs={props.deleteSongs} />
-            </tr>
-        })
-        return (
-            <div>
-                <h1>Music Library</h1>
-                <table>
-                    <tr>
-                        <th>Title</th>
-                        <th>Album</th>
-                        <th>Artist</th>
-                        <th>Genre</th>
-                        <th>Release Date</th>
-                        <th>Likes</th>
+    return (
+        <div class="container">
+            <h2>My Music Library</h2>
+            <table class="table table-dark table-striped">
+                <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Song</th>
+                    <th>Artist</th>
+                    <th>Album</th>
+                    <th>Release Date</th>
+                </tr>
+                </thead>
+                <tbody>
+                {props.songs.map((song, index) => (
+                    <tr data-index={index} key={song.id}>
+                        <th scope="row">{song.id}</th>
+                        <td>{song.title}</td>
+                        <td>{song.artist}</td>
+                        <td>{song.album}</td>
+                        <td>{song.release_date}</td>
+                        <Delete songid={song.id} deleteSongs={props.deleteSongs} />
                     </tr>
-                    <tbody>
-                    {musicList}
-                    </tbody>
-                </table>
-            </div>
-        );
-    }
+                ))}
+                </tbody>
+            </table>
+        </div>
+    );
 }
 
 export default MusicTable;
